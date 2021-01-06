93404eb784ef3bac1bae97c0c69c1ed3f190388f
jerrybear16 <jeroenwolfe@gmail.com>
2021-01-06 15:44:19 -0500

    added a second mapper
    program now runs in 2 modes which require command line argument

--- src/main/scala/WikiMapper2.scala
+++ src/main/scala/WikiMapper2.scala
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.LongWritable
 import org.apache.hadoop.io.IntWritable
 import org.apache.hadoop.io.Text
 
-class WikiMapper extends Mapper[LongWritable, Text, Text, IntWritable]{
+class WikiMapper2 extends Mapper[LongWritable, Text, Text, IntWritable]{
 
     override def map(
       key: LongWritable,
@@ -15,12 +15,12 @@ class WikiMapper extends Mapper[LongWritable, Text, Text, IntWritable]{
     val line = value.toString
 
     val lineArray = line.split("\\s").filter(_.length > 0)
-    var destination = ""
+    var origin = ""
     var checker = false 
       if (lineArray(2) == "link") checker = true
       if (checker == true)  {
-        destination = lineArray(1)
-        context.write(new Text(destination), new IntWritable(1))
+        origin = lineArray(0)
+        context.write(new Text(origin), new IntWritable(1))
         checker = false
       }
   }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -14,12 +14,12 @@ import org.apache.hadoop.io.IntWritable
 object Main extends App{
     
     if(args.length !=2){
-        println("Usage: Main [input dir] [output dir]")
+        println("Usage: Main [input dir] [output dir] [mode]")
+        println("for mode input 1 for Destination Counter, 2 for Source Counter")
         System.exit(-1)
     }
 
     val job = Job.getInstance()
-
     job.setJarByClass(Main.getClass())
     job.setJobName("wiki")
     job.setInputFormatClass(classOf[TextInputFormat])
@@ -27,7 +27,15 @@ object Main extends App{
     FileInputFormat.setInputPaths(job, new Path(args(0)))
     FileOutputFormat.setOutputPath(job, new Path(args(1)))
     
-    job.setMapperClass(classOf[WikiMapper])
+    if(args(2).equals("1")){
+        job.setMapperClass(classOf[WikiMapper])
+    }else if(args(2).equals("2")){
+        job.setMapperClass(classOf[WikiMapper2])
+    }else{
+        println("Invalid mode input")
+        System.exit(2)
+    }
+    
     job.setReducerClass(classOf[WikiReducer])
 
     job.setOutputKeyClass(classOf[Text])
